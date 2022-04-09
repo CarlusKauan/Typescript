2912b0f57246b83fdb879527d6c20b78573e1391
carloskauan <carloskauan190@gmail.com>
2022-04-09 12:31:02 -0300

    Criando ts !

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-// string, bollean, number
-let x:number = 10;
-x = 20
-console.log(x);
+// tipos básicos: string, bollean, number
+let myname:string = "Carlos";
+let myheight:number = 1.60;
+let happy:boolean = true;
+
+// inferencia x annotation
+let y = 12
+let z:number = 14
+
+// String != string
+console.log(typeof(myname) + "->" + typeof(myheight) + "->" + typeof(happy))
+
+// object
+const myNumbers: number[] = [10,20,30,50]
+const myNumberJs = [10,20,30]
+myNumberJs.push(100)
+
+
+console.log(myNumberJs)
+console.log("Qtde: " + myNumberJs.length)
+console.log(myNumbers)
+console.log("Qtde: " + myNumbers.length)
+
+// console.log()
+console.log("Meu nome é: " + myname + " Minha altura "  + myheight + " Estou feliz? " + happy);
+
+
+
 
