63f7fae649c2cd8058d9054326ead594277f0775
carloskauan <carloskauan190@gmail.com>
2022-04-09 12:30:46 -0300

    Compilação ts, gerando js !

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,20 @@
 "use strict";
-// string, bollean, number
-let x = 10;
-x = 20;
-console.log(x);
+// tipos básicos: string, bollean, number
+let myname = "Carlos";
+let myheight = 1.60;
+let happy = true;
+// inferencia x annotation
+let y = 12;
+let z = 14;
+// String != string
+console.log(typeof (myname) + "->" + typeof (myheight) + "->" + typeof (happy));
+// object
+const myNumbers = [10, 20, 30, 50];
+const myNumberJs = [10, 20, 30];
+myNumberJs.push(100);
+console.log(myNumberJs);
+console.log("Qtde: " + myNumberJs.length);
+console.log(myNumbers);
+console.log("Qtde: " + myNumbers.length);
+// console.log()
+console.log("Meu nome é: " + myname + " Minha altura " + myheight + " Estou feliz? " + happy);
